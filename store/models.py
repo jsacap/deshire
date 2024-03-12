85ac7f2b81980780a17b734b3a17b2c4b5afb521
jsacap <sanchoalegre@gmail.com>
2024-03-12 18:27:51 +1100

    models defined

--- store/models.py
+++ store/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 
-def customer(models.Model):
+def Customer(models.Model):
     
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
@@ -18,11 +22,8 @@ def customer(models.Model):
     MEMBERSHIP_CHOICES = [
         (MEMBERSHIP_BRONZE, 'Bronze'),
         (MEMBERSHIP_SILVER, 'Silver'),
-        (MEMBERSHIP_GOLD, 'GOLD'),
-        
-    ]
-    
-    
+        (MEMBERSHIP_GOLD, 'GOLD'),        
+    ]        
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=254, unique=True)
@@ -44,3 +45,24 @@ class Order(models.Model):
     
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, default=PAYMENT_STATUS_PENDING, choices=PAYMENT_STATUS_CHOICES)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal=2) # Unit price here to capture the price at the time it was ordered
+    
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length = 255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
